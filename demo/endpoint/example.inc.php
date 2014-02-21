f223f674db11e2671df376522597415ab7c42719
Woody <woodyrew@users.noreply.github.com>
2014-02-21 14:05:27 +0000

    Create example.inc.php

---
+++ demo/endpoint/example.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+class example {
+
+	protected function process_request ($pa_route_params, $pa_params, $ps_method) {
+		$la_rtn = array(
+			'route_params' => $pa_route_params
+		  , 'request_params' => $pa_params
+		);
+		$la_rtn['method'] = $ps_method;
+		return $la_rtn;
+	}
+	
+	public function get ($pa_route_params, $pa_params) {
+		return process_request($pa_route_params, $pa_params, 'get');
+	}
+	
+	public function get_all ($pa_route_params, $pa_params) {
+		return process_request($pa_route_params, $pa_params, 'get_all');
+	}
+	
+	public function add ($pa_route_params, $pa_params) {
+		return process_request($pa_route_params, $pa_params, 'add');
+	}
+	
+	public function edit ($pa_route_params, $pa_params) {
+		return process_request($pa_route_params, $pa_params, 'edit');
+	}
+	
+	public function remove ($pa_route_params, $pa_params) {
+		return process_request($pa_route_params, $pa_params, 'remove');
+	}
+}

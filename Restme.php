bc87af113568bf3b12079249dcdef07bdc8cecdc
Woody <woodyrew@users.noreply.github.com>
2014-02-24 13:25:44 +0000

    Added namespace

--- Restme.php
+++ Restme.php
@@ -15,7 +15,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-class Restme {
+namespace Restme;
+
+class Http {
 
 	private $endpoints = array();
 	private $route_params = array();
